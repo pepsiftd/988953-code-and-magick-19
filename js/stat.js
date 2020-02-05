f87fc7c029c05016a23b94a76c731696cd891aad
pepsiftd <pepsiftd@gmail.com>
2020-02-05 21:38:37 +0300

    Добавляет комментарии и исправляет ошибки линтера

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,18 @@
 'use strict';
 
 var drawBar = function (ctx, x, height, color, name, time) {
+  // высчитываем координату от края canvas-а
   x += 150;
+  // рисуем прямоугольник
   ctx.fillStyle = color;
   ctx.fillRect(x, 240 - height, 40, height);
+  // подписываем имя снизу и время сверху
   ctx.fillStyle = 'black';
   ctx.fillText(name, x, 260, 60);
   ctx.fillText(Math.round(time), x, 230 - height);
 };
 
+// функция нахождения наибольшего значения в массиве
 var getMaxOfArray = function (numArray) {
   return Math.max.apply(null, numArray);
 };
@@ -23,7 +27,7 @@ var renderStatistics = function (ctx, names, times) {
   ctx.fillRect(100, 10, 420, 270);
   ctx.strokeRect(100, 10, 420, 270);
 
-  // текст
+  // текст в окне победы
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
@@ -34,10 +38,10 @@ var renderStatistics = function (ctx, names, times) {
   var hiScore = getMaxOfArray(times);
 
   for (var i = 0; i < names.length; i++) {
-      var columnX = i * 90;
-      var columnHeight = diagramHeight * times[i] / hiScore;
-      var color = names[i] == 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(234, 100%, ' + Math.random() * 100 + '%)';
+    var columnX = i * 90; // координата от левого края гистограммы
+    var columnHeight = diagramHeight * times[i] / hiScore; // высота колонки в долях от максимального значения
+    var color = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(234, 100%, ' + Math.random() * 100 + '%)';
 
-      drawBar(ctx, columnX, columnHeight, color, names[i], times[i]);
+    drawBar(ctx, columnX, columnHeight, color, names[i], times[i]);
   }
 };
